99163a64bd19be8524edd06c78e909ec855cb5b3
devholic : MBPr (Mac) <devholic@plusquare.com>
2015-09-02 20:09:22 +0900

    fix error

--- nodejs/t2_task.js
+++ nodejs/t2_task.js
@@ -2,7 +2,7 @@ var amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://guest:guest@localhost:5672', function(err, conn) {
 	conn.createChannel(function(err, ch) {
-		var q = 'testQ';
+		var q = 'testQ_2';
 		var msg = process.argv.slice(2).join(' ') || "Hello World!";
 		ch.assertQueue(q, {durable: false});
 		ch.sendToQueue(q, new Buffer(msg), {persistent: true});
